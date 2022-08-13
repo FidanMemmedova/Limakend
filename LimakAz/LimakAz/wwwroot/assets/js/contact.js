5e11429d6406e035a0336c656913a90fb59f276d
FidanMemmedova <5TAWDL9@code.edu.az>
2022-08-13 14:35:05 +0400

    first

---
+++ LimakAz/LimakAz/wwwroot/assets/js/contact.js
@@ -0,0 +1,13 @@
+﻿//Location map display block/none content
+function mapFunction() {
+    var x = document.getElementById("contact-map");
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+    }
+    else
+    {
+    x.style.display = "none";
+    }
+}
+    
